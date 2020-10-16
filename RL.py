a321e9ac22e410f25188167beae0841d45370de0
Chris Werner-Westoby <56901476+chris-ww@users.noreply.github.com>
2020-10-15 20:44:59 -0400

    renders properly

--- RL.py
+++ RL.py
@@ -1,3 +1,11 @@
 import gym
 import gym_chrisww
 env = gym.make('chrisww-v0')
+env.reset()
+for _ in range(500):
+    env.render()
+    env.step(env.action_space.sample())
+env.close()
+# 2. To check all env available, uninstalled ones are also shown
+from gym import envs 
+print(envs.registry.all()
---
+++ baseline.py
@@ -0,0 +1,9 @@
+env = gym.make('CartPole-v0')
+
+env.reset()
+for _ in range(1000):
+    env.render()
+    obs, rew, done, info = env.step(env.action_space.sample()) # take a random action
+    if done:
+       env.reset()
+env.close()
