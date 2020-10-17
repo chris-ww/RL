1d40a234e0fd895862428349f5292bab4d79f67e
Chris Werner-Westoby <56901476+chris-ww@users.noreply.github.com>
2020-10-17 15:12:22 -0400

    DQN

--- RL.py
+++ RL.py
@@ -1,11 +1,30 @@
 import gym
 import gym_chrisww
+from stable_baselines.common.evaluation import evaluate_policy
+from stable_baselines import ACER
+from stable_baselines.common.policies import FeedForwardPolicy
+
+
+#Random Policy
 env = gym.make('chrisww-v0')
-env.reset()
+state=env.reset()
 for _ in range(500):
     env.render()
-    env.step(env.action_space.sample())
+    state,reward,done=env.step(env.action_space.sample())
 env.close()
+
+
+
+#DQN Policy
+
+# Train the agent
+model = DQN('MlpPolicy', env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
+model.learn(total_timesteps=int(2e5))
+
 # 2. To check all env available, uninstalled ones are also shown
-from gym import envs 
-print(envs.registry.all()
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+env.close()
