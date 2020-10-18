7f6b95f48e4c0a1a1dca4c3bab7bc7c9c832e5c1
Chris Werner-Westoby <56901476+chris-ww@users.noreply.github.com>
2020-10-17 23:38:20 -0400

    for now

--- RL.py
+++ RL.py
@@ -1,30 +1,83 @@
 import gym
 import gym_chrisww
 from stable_baselines.common.evaluation import evaluate_policy
-from stable_baselines import ACER
+from stable_baselines import DQN
+import tensorflow as tf
 from stable_baselines.common.policies import FeedForwardPolicy
+from stable_baselines.common.policies import ActorCriticPolicy
+from stable_baselines.common.policies import nature_cnn
+from stable_baselines.common.policies import mlp_extractor
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines import A2C
+from stable_baselines import PPO2
+from stable_baselines.gail import generate_expert_traj
+from stable_baselines.gail import ExpertDataset
+from sklearn.model_selection import RandomizedSearchCV
 
+#Demonstration
+def human_expert(_obs):
+    env.render()
+    key=input()
+    thisdict = {
+        "w": 3,
+        "a": 1,
+        "s": 2,
+        "d": 0
+        }
+    if key in thisdict:
+        return thisdict[key]
+    else:
+        return env.action_space.sample()
+    
+#Demonstration
+def simple_expert(_obs):
+    state=np.reshape(_obs,(5,5))
+    x1,y1=np.where(state==1)
+    x2,y2=np.where(state==2)
+    if(x1>x2):
+        return 1
+    if(y1>y2):
+        return 3
+    if(x1<x2):
+        return 0
+    else:
+        return 2
+
+
+
+generate_expert_traj(simple_expert, 'expert_game', env, n_episodes=10000)
+
+dataset = ExpertDataset(expert_path= 'expert_game.npz',
+                        traj_limitation=1, batch_size=100)
 
 #Random Policy
 env = gym.make('chrisww-v0')
-state=env.reset()
-for _ in range(500):
+env.reset()
+for _ in range(50):
     env.render()
-    state,reward,done=env.step(env.action_space.sample())
+    state,reward,done,info=env.step(env.action_space.sample())
+    print(reward)
 env.close()
 
 
 
 #DQN Policy
-
-# Train the agent
-model = DQN('MlpPolicy', env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
-model.learn(total_timesteps=int(2e5))
+model = A2C("MlpPolicy", env, verbose=1,learning_rate=5e-4)
+model.pretrain(dataset, n_epochs=5)
+model.learn(total_timesteps=int(1e4))
 
 # 2. To check all env available, uninstalled ones are also shown
 obs = env.reset()
-while True:
+for _ in range(500):
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     env.render()
+    if dones==True:
+        obs = env.reset()  
 env.close()
+
+
+
+state,reward,done,info=env.step(env.action_space.sample())
+env.render()
+print(reward)
