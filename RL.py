405c697ee20a79d80dc5cf7751edfd949f03934e
Chris Werner-Westoby <56901476+chris-ww@users.noreply.github.com>
2020-10-19 18:48:28 -0400

    RL solves level 1

--- RL.py
+++ RL.py
@@ -14,6 +14,8 @@ from stable_baselines.gail import generate_expert_traj
 from stable_baselines.gail import ExpertDataset
 import numpy as np
 import pygame
+from gym.envs.registration import register
+
 #from sklearn.model_selection import RandomizedSearchCV
 
 #Demonstration
@@ -33,7 +35,7 @@ def human_expert(_obs):
     
 #Demonstration
 def simple_expert(_obs):
-    state=np.reshape(_obs,(5,5,2))
+    state=np.reshape(_obs,(10,10,2))
     x1,y1=np.where(state[:,:,0]==True)
     x2,y2=np.where(state[:,:,1]==True)
     if(x1>x2):
@@ -45,32 +47,9 @@ def simple_expert(_obs):
     else:
         return 2
 
-
-
-generate_expert_traj(simple_expert, 'expert_game', env, n_episodes=1000)
-
-dataset = ExpertDataset(expert_path= 'expert_game.npz',
-                        traj_limitation=1, batch_size=100)
-
-#Random Policy
-env = gym.make('chrisww-v0')
-env.reset()
-for _ in range(50):
-    env.render()
-    state,reward,done,info=env.step(env.action_space.sample())
-    print(reward)
-env.close()
-
-
-
-#DQN Policy
-model = DQN("MlpPolicy", env, verbose=1,learning_rate=5.5e-4)
-model.pretrain(dataset, n_epochs=10000)
-model.learn(total_timesteps=int(2e3))
-
-def demonstrate():
+def demonstrate(env,model,iter):
     obs = env.reset()
-    for _ in range(500):
+    for _ in range(iter):
         action, _states = model.predict(obs)
         obs, rewards, dones, info = env.step(action)
         env.render()
@@ -78,31 +57,53 @@ def demonstrate():
             obs = env.reset()  
     env.close()
 
-def play_game():
-    done=False
-    obs = env.reset()
-    while(done==False):
-        env.render()
-        pressed_keys = pygame.key.get_pressed()
-        thisdict = {
-        "w": 3,
-        "a": 1,
-        "s": 2,
-        "d": 0
-        }
-        action=4
-        if pressed_keys[pygame.K_w]:
-            action=3
-        elif pressed_keys[pygame.K_s]:
-            action=2
-        elif pressed_keys[pygame.K_a]:
-            action=1
-        elif pressed_keys[pygame.K_d]:
-            action=0
-        print(sum(pressed_keys))        
-        obs, rewards, done, info = env.step(action)
-        env.render()
+def play_game(env,iter):
+    for i in range(iter):
+        done=False
+        obs = env.reset()
+        while(done==False):
+            env.render()
+            pressed_keys = pygame.key.get_pressed()
+            thisdict = {
+            "w": 3,
+            "a": 1,
+            "s": 2,
+            "d": 0
+            }
+            action=4
+            if pressed_keys[pygame.K_w]:
+                action=3
+            elif pressed_keys[pygame.K_s]:
+                action=2
+            elif pressed_keys[pygame.K_a]:
+                action=1
+            elif pressed_keys[pygame.K_d]:
+                action=0    
+            obs, rewards, done, info = env.step(action)
     env.close()
 
+
+
+
+#Random Policy
+def play_random(env,iter):
+    state=env.reset()
+    for i in range(iter):
+        env.render()
+        state,reward,done,info=env.step(env.action_space.sample())
+    env.close()
+
+#level_0= gym.make('chrisww-v0' )
+level_1= gym.make('chrisww-v1')
+#DQN Policy
+model_1 = DQN("MlpPolicy", level_1, verbose=1,learning_rate=1e-4,exploration_fraction=0.2)
+model_1.pretrain(dataset,learning_rate=0.00001, n_epochs=100)
+model_1.learn(total_timesteps=int(1e4))
+
+
+generate_expert_traj(simple_expert, 'expert_game', level_1, n_episodes=10000)
+
+dataset = ExpertDataset(expert_path= 'expert_game.npz',
+                        traj_limitation=-1, batch_size=100)
 
 
